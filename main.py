06064d109dbcdf9cf33b2d429e63a727d96e5c32
PoonamKalita18 <arunakalita2015@gmail.com>
2024-10-10 21:33:23 +0530

    pyton

---
+++ main.py
@@ -0,0 +1,21 @@
+name = "Poonam"
+age = 16
+is_student = True
+weigth = 60.5
+
+print("name :", name)
+print("Data Type of Name is :",type(name))
+print("age :",age)
+print("Data Type of Age is :",type(age))
+print("is_student :", is_student)
+print("Data Type of is_student is :",type(is_student))
+print("weight:",weigth)
+print("Data Type of Weigth is :",type(weigth))
+
+print("\n After Type Casting....")
+age = str(age)
+print(age)
+print("Data Type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of Weight is", type(weight))
